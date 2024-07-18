a2c78a8621bf4e69ef20cddf7e7698a8b0025760
umut imamoglu <umutimamoglu19@gmail.com>
2024-07-18 18:06:46 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoute = require("./Routes/userRoute")
+
+const app = express();
+require("dotenv").config();
+
+app.use(express.json());
+app.use(cors());
+app.use("/api/users", userRoute)
+
+
+app.get("/", (req, res) => {
+
+});
+
+
+const port = process.env.PORT || 3505;
+const uri = process.env.ATLAS_URI;
+
+app.listen(port, (req, res) => {
+    console.log(`server running on port: ${port}`);
+});
+
+mongoose.connect(uri)
+    .then(() => console.log("MongoDB connection established"))
+    .catch((error) => console.log("MongoDb connection failed:", error.message));
